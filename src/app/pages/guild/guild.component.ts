551207c9bc1d64668a5147d1e53e4f7511ce214e
Kain <drkain@outlook.com>
2020-02-28 02:30:16 +1300

    More guild information
    + Readable....words?
    + Guild Resources
    + Building Levels
    + Crystal/Level Totals

--- src/app/pages/guild/guild.component.ts
+++ src/app/pages/guild/guild.component.ts
@@ -39,10 +39,47 @@ export class GuildComponent implements OnInit {
     return Object.keys(this.guild.members).length;
   }
 
+  public titleCase(str: string): string {
+    return str.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  }
+
+  public getValuesTotal(obj: any) {
+    let sum = 0;
+    Object.keys(obj).forEach(item => sum += obj[item]);
+    return sum;
+  }
+
+  // Used to convert object keys into readable strings. EG: factory:item -> Item Factory
+  public formatWord(str: string): string {
+    const [type, value] = str.split(':');
+
+    if (type.startsWith('Crystal')) {
+      return `${type.slice(7)} Crystals`;
+    }
+
+    switch (type) {
+      case 'guildhall': return 'Guild Hall';
+      case 'generator':
+      case 'factory': return `${value} ${type}`;
+      case 'garden': return `${value.toUpperCase()} ${type}`;
+      case 'upkept':
+      case 'person':
+      case 'active':
+        switch (value) {
+          case 'raidportal': return 'Raid Portal';
+          case 'witchdoctor': return 'Witch Doctor';
+          default: return value;
+        }
+      default: return value ? `${type}:${value}` : type;
+    }
+  }
+
   public symbolFor(guildLv: number): string {
     switch (guildLv) {
-      case 1:  return '';
-      case 5:  return '☆';
+      case 1: return '';
+      case 5: return '☆';
       case 10: return '★';
       default: return '';
     }
